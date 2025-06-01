5d0272c6992bc55c12923f6667aab6d6a58d8499
FlorenzaJ <03081230079@student.uph.edu>
2025-06-02 01:11:08 +0700

    First Commit Kalkulator

---
+++ app/src/main/java/edu/uph/m23si1/myapplication/KalkulatorSederhana.java
@@ -0,0 +1,31 @@
+package edu.uph.m23si1.myapplication;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class KalkulatorSederhana extends AppCompatActivity {
+
+    Button buttonTambah, buttonKali, buttonBagi;
+    EditText editText1, editText2;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_kalkulator_sederhana);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ app/src/main/java/edu/uph/m23si1/myapplication/RegisterActivity.java
@@ -0,0 +1,38 @@
+package edu.uph.m23si1.myapplication;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        final Button button = findViewById(R.id.btnRegister);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(),"Tombol Register diTekan", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.BOTTOM | Gravity.RIGHT, 0, 0);
+                toast.show();
+            }
+        });
+
+    }
+}
